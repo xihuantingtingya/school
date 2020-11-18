3e729d07cdb78632b90d7b2df4ef92273f292c83
covt48 <74312219+covt48@users.noreply.github.com>
2020-11-18 16:56:20 +0800

    Create 1118工资等级划分.c

---
+++ 1118工资等级划分.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main(){
+int n;
+scanf("%d",&n);
+if(n<=-1){
+printf("你还有花呗没有还,无法使用该程序");
+}
+else if(n>=0 && n<=500)
+{
+printf("赤贫");
+}
+else if(n>=501 && n<=1500)
+{
+printf(" 普通打工人");
+}
+else if(n>=1501 && n<=3000)
+{
+printf("普通清洁工");
+}
+else if(n>=3001 && n<=5000)
+{
+printf("水电工工资");
+}
+else if(n>=50001 && n<=12000)
+{
+printf(" 脑袋大脖子粗不是老板就是伙夫");
+}
+else if(n>=50001 && n<=2000000000)
+{
+printf(" 程序员");
+}
+}
