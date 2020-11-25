3b3c6045b0549528a1e8bfa74438896d0fe6ebb4
covt48 <74312219+covt48@users.noreply.github.com>
2020-11-25 16:59:14 +0800

    Create 112503.模拟延时程序.c

---
+++ 112503.模拟延时程序.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+printf("天气冷了，有句成语叫做忘穿秋裤，你今天穿秋裤了吗？");
+Sleep(3000);
+printf("没有穿。");
+Sleep(5000);
+printf("为什么不穿呢？");
+Sleep(4000);
+printf("广西人不需要穿秋裤。");
+}
