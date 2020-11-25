a7e008841f9d148da9dca38d815e4a8cde64e54c
covt48 <74312219+covt48@users.noreply.github.com>
2020-11-25 09:54:35 +0800

    Create 112502.qs排名查询.app.c

---
+++ 112502.qs排名查询.app.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main(){
+int n;
+scanf("%d",&n);
+if(n=1){
+printf("清华大学");
+}
+else if(n=2)
+{
+printf("北京大学");
+}
+else if(n=3)
+{
+printf("复旦大学");
+}
+else if(n=4)
+{
+printf("中国科学技术大学");
+}
+else if(n=5)
+{
+printf("浙江大学");
+}
+else if(n=6)
+{
+printf("上海交通大学");
+}
+else if(n=7)
+{
+printf("南京大学");
+}
+else if(n=8)
+{
+printf("中山大学");
+}
+else if(n=9)
+{
+printf("武汉大学");
+}
+else if(n=10)
+{
+printf("哈尔滨工业大学");
+}
+else
+printf("老铁的排名太靠后，请重新输入");
+}
