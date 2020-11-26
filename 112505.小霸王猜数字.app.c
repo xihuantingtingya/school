37b7c3662a0a8d99d30040bd514670dbce541833
covt48 <74312219+covt48@users.noreply.github.com>
2020-11-26 08:47:10 +0800

    Create 112505.小霸王猜数字.app.c

---
+++ 112505.小霸王猜数字.app.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<windows.h>
+void main()
+{
+while(1)
+{
+{
+printf("友情提示；请输入0-10");
+}
+int a=5;
+int b;
+scanf("%d",&b);
+if(b==a)
+{
+printf("恭喜你猜对了。");
+}
+else if(b>a)
+{
+printf("你猜的太大了。"); 
+}
+else
+printf("你猜的太小了。"); 
+}
+}
