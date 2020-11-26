6e1f1f4d5727c14cb64e8f46d2947f765b8b9ddd
covt48 <74312219+covt48@users.noreply.github.com>
2020-11-26 08:42:31 +0800

    Create 112504.秒表计时器.app.c

---
+++ 112504.秒表计时器.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+while(1)
+{
+printf("现在第一秒钟。\n");
+Sleep(1000);
+printf("现在第二秒钟。\n");
+Sleep(1000);
+printf("现在第三秒钟。\n");
+Sleep(1000);
+printf("现在第四秒钟。\n");
+}
+return 0;
+}
