3367e1e59435a088534c3f9f6b5bf5a172613c57
covt48 <74312219+covt48@users.noreply.github.com>
2020-12-02 16:54:05 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -6,7 +6,7 @@ scanf("%d",&a);
 b=a/3600;
 c=(a%3600)/60;
 d=a-3600*b-60*c;
-printf("%d时",b);
-printf("%d分",c);
-printf("%d秒",d);
+printf("%dhour",b);
+printf("%dminute",c);
+printf("%dsecond",d);
 }
