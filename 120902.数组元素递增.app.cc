e0a5d797a48b62477849c85c5be796e344e16294
covt48 <74312219+covt48@users.noreply.github.com>
2020-12-09 10:43:25 +0800

    Update and rename 120902.数组元素的递增.app.cc to 120902.数组元素递增.app.cc

--- 120902.数组元素递增.app.cc
+++ 120902.数组元素递增.app.cc
@@ -3,10 +3,10 @@ void main()
 {
 	int value=0;
 	int a,b;
-	int array2d[9][20]={
+	int array2d[19][20]={
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	};
-for(a=0;a<9;a++){
+for(a=0;a<19;a++){
 	for(b=0;b<20;b++){
 		value++;
 		array2d[a][b]=value;
