7d4ef3441094cb11882c4ae0df8c7b95e0ca94f1
covt48 <74312219+covt48@users.noreply.github.com>
2020-12-09 09:46:12 +0800

    Create 120902.数组元素的递增.app.cc

---
+++ 120902.数组元素的递增.app.cc
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int value=0;
+	int a,b;
+	int array2d[9][20]={
+		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	};
+for(a=0;a<9;a++){
+	for(b=0;b<20;b++){
+		value++;
+		array2d[a][b]=value;
+		printf("%d ",array2d[a][b]);
+}
+	printf("\n");
+}
+}
