4eeac3496184349d6d5254a13910d0fb169d84a9
covt48 <74312219+covt48@users.noreply.github.com>
2020-12-09 17:14:56 +0800

    Create 120905. 串长度.app.c

---
+++ 120905. 串长度.app.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char s1[]="I am a lovely girl";
+    char s2[]="不忘初心，牢记使命";
+	printf("%d",sizeof(s1));
+	printf("\n");
+	printf("%d",sizeof(s2));
+	printf("\n");
+}
