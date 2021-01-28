026cfa612f8a533864cce6ea2e99589b7252e52b
xihuantingtingya <74294975+xihuantingtingya@users.noreply.github.com>
2021-01-28 21:50:18 +0800

    Update 112502.qs排名查询.app.c

--- 112502.qs排名查询.app.c
+++ 112502.qs排名查询.app.c
@@ -2,42 +2,42 @@
 void main(){
 int n;
 scanf("%d",&n);
-if(n=1){
+if(n==1){
 printf("清华大学");
 }
-else if(n=2)
+else if(n==2)
 {
 printf("北京大学");
 }
-else if(n=3)
+else if(n==3)
 {
 printf("复旦大学");
 }
-else if(n=4)
+else if(n==4)
 {
 printf("中国科学技术大学");
 }
-else if(n=5)
+else if(n==5)
 {
 printf("浙江大学");
 }
-else if(n=6)
+else if(n==6)
 {
 printf("上海交通大学");
 }
-else if(n=7)
+else if(n==7)
 {
 printf("南京大学");
 }
-else if(n=8)
+else if(n==8)
 {
 printf("中山大学");
 }
-else if(n=9)
+else if(n==9)
 {
 printf("武汉大学");
 }
-else if(n=10)
+else if(n==10)
 {
 printf("哈尔滨工业大学");
 }
